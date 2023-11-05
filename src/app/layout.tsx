1285ed40ce478d45a02fa14a29f65f286a830b28
RostyslavKr <106476898+RostyslavKr@users.noreply.github.com>
2023-11-05 21:32:12 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Rosyslav Kryvoviaz | Frontend Developer',
+  title: 'Rostyslav Kryvoviaz | Frontend Developer',
   description: "I'm frontend developer",
 }
 
